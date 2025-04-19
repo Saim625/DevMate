3e12bff86fc1113f1564156c73b3a3a54ad8be95
saim625 <siamsaeed625@gmail.com>
2025-04-19 14:09:55 +0500

    /api included in redirect url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ const plans = {
 };
 
 const payfast_redirect_url = (selectedPlan, orderId) => {
-  return `https://sandbox.payfast.co.za/eng/process?merchant_id=${process.env.MERCHANT_ID}&merchant_key=${process.env.MERCHANT_KEY}&amount=${selectedPlan.price}&item_name=${selectedPlan.name}&return_url=http://localhost:5173/thank-you&cancel_url=http://localhost:5173/cancel&notify_url=https://devmate.click/payment/webhook&custom_str1=${orderId}`;
+  return `https://sandbox.payfast.co.za/eng/process?merchant_id=${process.env.MERCHANT_ID}&merchant_key=${process.env.MERCHANT_KEY}&amount=${selectedPlan.price}&item_name=${selectedPlan.name}&return_url=http://localhost:5173/thank-you&cancel_url=http://localhost:5173/cancel&notify_url=https://devmate.click/api/payment/webhook&custom_str1=${orderId}`;
 };
 
 module.exports = { plans, payfast_redirect_url };
